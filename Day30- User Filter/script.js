ebbec2ea428701fc0af0fefd7bc841faf8624c0a
Randhir Shaw <randhirshaw10@gmail.com>
2022-03-10 01:52:27 +0530

    Update script.js

--- Day30- User Filter/script.js
+++ Day30- User Filter/script.js
@@ -10,8 +10,7 @@ async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
 
     const { results } = await res.json()
-
-    // Clear result
+    
     result.innerHTML = ''
 
     results.forEach(user => {
